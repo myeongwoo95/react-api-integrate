fe83381fc48b9d60973f694701c71a4cb8852e2c
walle <dlauddn95@gmail.com>
2021-12-10 00:54:35 +0900

    :>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Users from "./Users";
+import { UsersProvider } from "./UserContext";
 
 function App() {
-    return <Users />;
+    return (
+        <UsersProvider>
+            <Users />
+        </UsersProvider> 
+    )
 }
 
 export default App;
--- src/Users.js
+++ src/Users.js
@@ -1,25 +1,21 @@
 import React, { useState} from 'react';
-import axios from 'axios';
-import { useAsync } from 'react-async';
 import User from './User';
-
-// useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
-// 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
-async function getUsers(){
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-    return response.data;
-}
+import { getUsers, useUsersDispatch, useUsersState } from './UserContext';
 
 function Users(){
     const [userId, setUserId] = useState(null);
-    const { isLoading, data: users, error, /*reload*/ run} = useAsync({
-        //promiseFn: getUsers
-        deferFn: getUsers
-    })
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
 
-    if (isLoading) return <div>로딩중..</div>;
-    if (error) return <div>에러가 발생했습니다</div>;
-    if (!users) return <button onClick={run}>불러오기</button>;
+    const {loading , data: users, error} = state.users;
+
+    const fetchData = () => {
+        getUsers(dispatch);
+    }
+
+    if (loading) return <div>로딩중..</div>
+    if (error) return <div>에러가 발생했습니다</div>
+    if (!users) return <button onClick={fetchData}>불러오기</button>
     
     return(
         <>
@@ -34,7 +30,7 @@ function Users(){
                     </li>
                 ))}
             </ul>
-            <button onClick={run}>다시 불러오기</button>
+            <button onClick={fetchData}>다시 불러오기</button>
             {userId && <User id={userId} />}
         </>
     );
